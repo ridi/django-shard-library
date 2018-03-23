d30c1a316eb194619fe4ebba2d7ae58dca830d03
김정근 <snorlax@ridi.com>
2018-03-23 14:47:58 +0900

    Prepare to deploy 0.0.3

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,9 @@
 Changelog
 =========
+0.0.3 (Mar 23th 2018)
+------------------
+- Change logic for calculate shard_key
+- Support raw, bulk_create in `ShardManager`
 
 0.0.2 (Mar 23th 2018)
 ------------------
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.0.2'
+version = '0.0.3'
 
 with open('requirements/base.txt') as f:
     install_requires = [line for line in f if line and not line.startswith('-')]
