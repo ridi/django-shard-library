01c5c679dd16bee9130f95685508ba5514bb889f
김정근 <snorlax@ridi.com>
2018-03-23 14:46:06 +0900

    Change logic for calc shard_key

--- shard/managers.py
+++ shard/managers.py
@@ -1,7 +1,9 @@
+import typing
+
 from django.db.models.manager import Manager
 
 from shard.queryset import ShardQuerySet
-from shard.utils.shard_key import get_shard_key_from_kwargs
+from shard.utils.shard_key import get_shard_key_from_kwargs, mod_shard_key_by_replica_count
 
 __all__ = ('ShardManager', )
 
@@ -17,11 +19,18 @@ def _wrap(func_name):
 
 class ShardManager(Manager):
     def shard(self, shard_key):
-        return self.get_queryset(shard_key=shard_key)
+        _shard_key = mod_shard_key_by_replica_count(shard_key, self.model.shard_group)
+        return self.get_queryset(shard_key=_shard_key)
 
     def get_queryset(self, shard_key=None):
         hints = {'shard_key': shard_key}
         return ShardQuerySet(model=self.model, hints=hints)
+
+    def raw(self, shard_key: int, query: str):
+        return self.shard(shard_key=shard_key).raw(query)
+
+    def bulk_create(self, shard_key: int, objs: typing.List, batch_size: int):
+        return self.shard(shard_key=shard_key).bulk_create(objs=objs, batch_size=batch_size)
 
     filter = _wrap('filter')
     get = _wrap('get')
