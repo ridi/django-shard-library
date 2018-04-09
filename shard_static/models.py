a5167dae757634fda2cb6617f80bf5f2c6c0f019
김정근 <snorlax@ridi.com>
2018-04-09 14:35:08 +0900

    Initialize StaticSyncStatus in shard-static app

--- shard_static/models.py
+++ shard_static/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class ShardSyncStatus(models.Model):
+    static_model_key = models.CharField(max_length=64, verbose_name='Static Model Key')
+    last_modified = models.DateTimeField(null=True, verbose_name='Last Modified', )
+    is_locked = models.BooleanField(null=False, default=False, verbose_name='Is Locked')
+
+    class Meta:
+        db_table = 'shard_sync_status'
