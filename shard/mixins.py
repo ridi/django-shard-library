96909591019dad78a0bc18cd9389a96636b797ca
김정근 <snorlax@ridi.com>
2018-04-09 15:33:43 +0900

    Add comment for diffusible

--- shard/mixins.py
+++ shard/mixins.py
@@ -52,6 +52,8 @@ class ShardMixin:
 
 
 class ShardStaticMixin:
-    diffusible = True
-
     shard_group = ALL_SHARD_GROUP
+
+    # If difussible set true, exists source data in default
+    # If difussible set false, only exists in shard database
+    diffusible = True
