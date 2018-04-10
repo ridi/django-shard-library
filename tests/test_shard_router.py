ad480aa43ac9395d1521de66679ac1732268d759
김정근 <snorlax@ridi.com>
2018-04-10 19:10:13 +0900

    Add testcase for shard_router

--- tests/test_shard_router.py
+++ tests/test_shard_router.py
@@ -2,17 +2,12 @@ from django.test import TestCase
 from django_dynamic_fixture import G
 
 from shard.routers.shard import ShardRouter
-from tests.models import ShardModelA, NormalModel
+from tests.models import ShardModelA, NormalModel, ShardModelB, ShardStaticAll, ShardStaticA, ShardStaticB
 
 
 class ShardRouterTestCase(TestCase):
     '''
     # Allow_relation
-        # Two Shard objs (Same group, Other group)
-        # a normal model and a shard model
-        # One Shard obj and One Shard Static obj (Same group, Other group, all_group)
-
-        # Two Shard Static Objs (Same group and other group and all_group)
         # Not Shard objs (has not opnion)
     '''
 
@@ -20,9 +15,9 @@ class ShardRouterTestCase(TestCase):
         self.router = ShardRouter()
 
     def test_db_for_write_with_shard_object(self):
-        obj1 = G(ShardModelA, user_id=1, parent=None, static_same=None, static_all=None, static_other=None)
-        obj2 = G(ShardModelA, user_id=2, parent=None, static_same=None, static_all=None, static_other=None)
-        obj3 = G(ShardModelA, user_id=3, parent=None, static_same=None, static_all=None, static_other=None)
+        obj1 = G(ShardModelA, user_id=1)
+        obj2 = G(ShardModelA, user_id=2)
+        obj3 = G(ShardModelA, user_id=3)
 
         write_db1 = self.router.db_for_write(model=obj1.__class__, instance=obj1)
         write_db2 = self.router.db_for_write(model=obj2.__class__, instance=obj2)
@@ -36,3 +31,66 @@ class ShardRouterTestCase(TestCase):
         write_db1 = self.router.db_for_write(model=obj1.__class__, hints={'instance': obj1})
 
         self.assertIsNone(write_db1)
+
+    def test_allow_relations_with_two_shard_objects(self):
+        shard_a_obj1 = G(ShardModelA, user_id=1)
+        shard_a_obj2 = G(ShardModelA, user_id=2)
+        shard_a_obj3 = G(ShardModelA, user_id=3)
+
+        shard_b_obj1 = G(ShardModelB, user_id=1)
+
+        self.assertNotEqual(shard_a_obj1.shard_group, shard_b_obj1.shard_group)
+
+        self.assertFalse(self.router.allow_relation(shard_a_obj1, shard_a_obj2))
+        self.assertTrue(self.router.allow_relation(shard_a_obj1, shard_a_obj3))
+
+        self.assertFalse(self.router.allow_relation(shard_a_obj1, shard_b_obj1))
+        self.assertFalse(self.router.allow_relation(shard_a_obj2, shard_b_obj1))
+        self.assertFalse(self.router.allow_relation(shard_a_obj3, shard_b_obj1))
+
+    def test_allow_relation_with_a_shard_object_and_a_static_object(self):
+        shard_a_obj1 = G(ShardModelA, user_id=1)
+        shard_b_obj1 = G(ShardModelB, user_id=1)
+
+        shard_all_static = G(ShardStaticAll)
+        shard_a_static = G(ShardStaticA)
+
+        self.assertTrue(self.router.allow_relation(shard_a_obj1, shard_a_static))
+        self.assertFalse(self.router.allow_relation(shard_b_obj1, shard_a_static))
+
+        self.assertTrue(self.router.allow_relation(shard_a_obj1, shard_all_static))
+        self.assertTrue(self.router.allow_relation(shard_b_obj1, shard_all_static))
+
+    def test_allow_relation_with_a_normal_object_and_a_static_object(self):
+        shard_all_static = G(ShardStaticAll)
+        shard_a_static = G(ShardStaticA)
+        shard_b_static = ShardStaticB.objects.shard(shard_key=1).create()
+
+        normal_object = G(NormalModel)
+
+        self.assertTrue(self.router.allow_relation(normal_object, shard_all_static))
+        self.assertTrue(self.router.allow_relation(normal_object, shard_a_static))
+        self.assertFalse(self.router.allow_relation(normal_object, shard_b_static))
+
+        self.assertFalse(shard_b_static.diffusible)
+
+        # delete instance for isolation test
+        shard_b_static.delete()
+
+    def test_allow_relation_with_two_static_objects(self):
+        shard_all_static = G(ShardStaticAll)
+        shard_a_static = G(ShardStaticA)
+        shard_b_static = ShardStaticB.objects.shard(shard_key=1).create()
+
+        self.assertTrue(self.router.allow_relation(shard_a_static, shard_all_static))
+        self.assertTrue(self.router.allow_relation(shard_b_static, shard_all_static))
+        self.assertFalse(self.router.allow_relation(shard_a_static, shard_b_static))
+
+        # delete instance for isolation test
+        shard_b_static.delete()
+
+    def test_allow_relation_with_two_normal_objects(self):
+        normal_object1 = G(NormalModel)
+        normal_object2 = G(NormalModel)
+
+        self.assertIsNone(self.router.allow_relation(normal_object1, normal_object2))
