99e1934e8fa86731c97cfdf4b40f509e450eb117
김정근 <snorlax@ridi.com>
2018-04-12 16:07:49 +0900

    Add to testcase for lock service

--- runcommand.py
+++ runcommand.py
@@ -61,7 +61,7 @@ SETTINGS_DICT = {
         'shard_static',
     ],
     'DATABASE_ROUTERS': ['shard.routers.specific.SpecificRouter', 'shard_static.routers.ShardStaticRouter'],
-    'SHARD_SYNC_LOCK_MANAGER': 'tests.lock.FakeLockManager',
+    'SHARD_SYNC_LOCK_MANAGER_CLASS': 'tests.lock.FakeLockManager',
 }
 
 
--- shard_static/services/lock_service.py
+++ shard_static/services/lock_service.py
@@ -1,13 +1,13 @@
 from importlib import import_module
 from typing import Type
 
-from shard_static.config import SHARD_SYNC_LOCK_TTL, SHARD_SYNC_LOCK_MANAGER
+from shard_static import config
 from shard_static.lock import BaseLockManager
 
 
 def get_lock_manager(model_name: str, database_alias: str) -> BaseLockManager:
     lock_key = _make_lock_key(model_name, database_alias)
-    ttl = SHARD_SYNC_LOCK_TTL
+    ttl = config.SHARD_SYNC_LOCK_TTL
     lock_manager_class = _import_lock_manager_class()
 
     return lock_manager_class(key=lock_key, ttl=ttl)
@@ -18,7 +18,7 @@ def _make_lock_key(model_name: str, database_alias: str) -> str:
 
 
 def _import_lock_manager_class() -> Type[BaseLockManager]:
-    mod_path, _, cls_name = SHARD_SYNC_LOCK_MANAGER.rpartition('.')
+    mod_path, _, cls_name = config.SHARD_SYNC_LOCK_MANAGER_CLASS.rpartition('.')
     mod = import_module(mod_path)
     cls = getattr(mod, cls_name)
     return cls
