ac979e0dff13bc616a8d801c60b6c9536b5973c7
김정근 <snorlax@ridi.com>
2018-05-24 21:59:08 +0900

    Change hash limit from 10**8 to 2**64

--- shard/utils/consistent_hash/con_hash.py
+++ shard/utils/consistent_hash/con_hash.py
@@ -4,5 +4,5 @@ from typing import Any
 
 def con_hash(value: Any) -> int:
     value = str(value)
-    hash_value = int(hashlib.sha256(value.encode('utf-8')).hexdigest(), 16) % (10 ** 8)
+    hash_value = int(hashlib.sha256(value.encode('utf-8')).hexdigest(), 16) % (2 ** 64)
     return hash_value
