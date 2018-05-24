6dcd73ab4a4f337281d33292440d01836819a948
김정근 <snorlax@ridi.com>
2018-05-24 17:03:13 +0900

    Add testcase for con hash pool

--- shard/utils/shard_key.py
+++ shard/utils/shard_key.py
@@ -6,7 +6,7 @@ from shard.mixins import ShardMixin
 __all__ = ('get_shard_key_from_kwargs', 'get_shard_key_from_instance', 'mod_shard_key_by_replica_count', )
 
 SHARD_REPLICA_COUNT_SETTING = 'SHARD_REPLICA_COUNT_SETTING'
-DEFAULT_REPLICA_COUNT = 1024
+DEFAULT_REPLICA_COUNT = 128
 
 
 def get_shard_key_from_kwargs(model, **kwargs) -> int:
@@ -22,16 +22,16 @@ def get_shard_key_from_instance(instance: ShardMixin) -> int:
     return instance.get_shard_key()
 
 
-def mod_shard_key_by_replica_count(shard_key: int, shard_group: str) -> int:
-    return _calc_shard_key(shard_key, shard_group)
-
-
 def _validate_shard_model(model):
     if not issubclass(model, ShardMixin):
         raise NotShardingModelException()
 
     if model.shard_group is None or model.shard_key_name is None:
         raise NotShardingModelException()
+
+
+def mod_shard_key_by_replica_count(shard_key: int, shard_group: str) -> int:
+    return _calc_shard_key(shard_key, shard_group)
 
 
 def _calc_shard_key(key: int, shard_group: str) -> int:
