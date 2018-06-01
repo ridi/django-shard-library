b43e3ac8f67086bdfdc1504cbc23b684ebde8eff
김정근 <snorlax@ridi.com>
2018-06-01 19:02:36 +0900

    Change consistent hash algorithm

--- shard/utils/consistent_hash/con_hash.py
+++ shard/utils/consistent_hash/con_hash.py
@@ -2,7 +2,7 @@ import hashlib
 from typing import Any
 
 
-def md5_hash(value: Any) -> int:
+def blake2b_hash(value: Any) -> int:
     value = str(value)
-    hash_value = int(hashlib.md5(value.encode('utf-8')).hexdigest(), 16) % (2 ** 64)
+    hash_value = int(hashlib.blake2b(value.encode('utf-8')).hexdigest(), 16) % (2 ** 64)
     return hash_value
