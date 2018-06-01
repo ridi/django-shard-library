a1b0e3c732e32164bdb9588fab8c9808b327ee13
김정근 <snorlax@ridi.com>
2018-06-01 19:43:32 +0900

    Fix import

--- shard/utils/consistent_hash/con_hash.py
+++ shard/utils/consistent_hash/con_hash.py
@@ -1,8 +1,8 @@
-import hashlib
+from hashlib import blake2b
 from typing import Any
 
 
 def blake2b_hash(value: Any) -> int:
     value = str(value)
-    hash_value = int(hashlib.blake2b(value.encode('utf-8')).hexdigest(), 16) % (2 ** 64)
+    hash_value = int(blake2b(value.encode('utf-8')).hexdigest(), 16) % (2 ** 64)
     return hash_value
