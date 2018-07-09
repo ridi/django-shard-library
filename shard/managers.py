97bb7f197048c3e7a6a5af269f73e2ee9c471642
Junheong Park <jh.park@ridi.com>
2018-07-09 18:28:48 +0900

    Add is_fresh

--- shard/managers.py
+++ shard/managers.py
@@ -1,29 +1,35 @@
 import typing
-
 from django.db.models.manager import Manager
 
 from shard.queryset import ShardQuerySet
+from shard.strategy.routing.random import RandomReadStrategy
 from shard.utils.shard import get_shard_by_shard_key_and_shard_group
 from shard.utils.shard_key import get_shard_key_from_kwargs
 
-__all__ = ('ShardManager', )
+__all__ = ('ShardManager',)
 
 
 def _wrap(func_name):
     def wrapped(self, *args, **kwargs):
         shard_key = get_shard_key_from_kwargs(model=self.model, **kwargs)
-        return getattr(self.get_queryset(shard_key=shard_key), func_name)(*args, **kwargs)
+        is_fresh = kwargs.get('is_fresh', False)
+        return getattr(self.get_queryset(shard_key=shard_key, is_fresh=is_fresh), func_name)(*args, **kwargs)
 
     wrapped.__name__ = func_name
     return wrapped
 
 
 class BaseShardManager(Manager):
-    def shard(self, shard: str):
+    _strategy = RandomReadStrategy
+
+    def shard(self, shard: str, is_fresh: bool = False):
+        if not is_fresh:
+            shard = self._strategy.pick_read_db(shard)
+
         return self.get_queryset().using(shard)
 
-    def get_queryset(self, shard_key=None):
-        hints = {'shard_key': shard_key}
+    def get_queryset(self, shard_key=None, is_fresh: bool = False):
+        hints = {'shard_key': shard_key, 'is_fresh': is_fresh, }
         return ShardQuerySet(model=self.model, hints=hints)
 
 
@@ -32,7 +38,7 @@ class ShardManager(BaseShardManager):
         return self.shard(shard=get_shard_by_shard_key_and_shard_group(shard_key=shard_key, shard_group=self.model.shard_group)).raw(query)
 
     def bulk_create(self, shard_key: int, objs: typing.List, batch_size: int):
-        return self.shard(shard=get_shard_by_shard_key_and_shard_group(shard_key=shard_key, shard_group=self.model.shard_group))\
+        return self.shard(shard=get_shard_by_shard_key_and_shard_group(shard_key=shard_key, shard_group=self.model.shard_group)) \
             .bulk_create(objs=objs, batch_size=batch_size)
 
     filter = _wrap('filter')
