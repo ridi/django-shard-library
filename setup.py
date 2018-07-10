3f4c7bd89dafaed09a8eb47d404457a0cc8ffaaf
Junheong Park <jh.park@ridi.com>
2018-07-10 15:27:41 +0900

    FIx version in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-version = '0.0.18'
+version = '0.1.0'
 
 with open('requirements/base.txt') as f:
     install_requires = [line for line in f if line and not line.startswith('-')]
