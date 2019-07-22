00022f7d5f7ea76430ab06bc3d8cba597cb28e80
김정근 <snorlax@ridi.com>
2019-07-22 16:46:46 +0900

    setup by 수정

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-version = '0.1.2'
+version = '0.1.3'
 
 with open('requirements/base.txt') as f:
     install_requires = [line for line in f if line and not line.startswith('-')]
